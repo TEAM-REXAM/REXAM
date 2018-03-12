c915986a30a9206b4ccb3a3a39efb56924194d2e
adam-bekkouche <adam.bekkouche@hotmail.com>
2018-03-12 16:20:18 +0100

    boutons ajoutées pour discipline mais pas d'affichage d'ue

--- src/main/java/com/rexam/controller/StudentController.java
+++ src/main/java/com/rexam/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.rexam.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.rexam.dao.TeachingUnitRepository;
 import com.rexam.model.TeachingUnit;
@@ -15,8 +18,11 @@ public class StudentController {
 	TeachingUnitRepository tuRepository;
 
 	@RequestMapping("/showTeachingUnits")
-	public String home() {
-		return "teachingUnits";
+	public ModelAndView showDisciplines() {
+		List<String> disciplines = tuRepository.findDisciplines();
+		ModelAndView mav = new ModelAndView("teachingUnits");
+		mav.addObject("disciplines", disciplines);
+		return mav;
 	}
 
 	@RequestMapping("/addTeachingUnits")
@@ -27,6 +33,12 @@ public class StudentController {
 		u.setDiscipline("Sciences");
 		u.setCreditValue(3);
 		tuRepository.save(u);
+		TeachingUnit u2 = new TeachingUnit();
+		u2.setCode("UE2");
+		u2.setName("philo");
+		u2.setDiscipline("Lettres");
+		u2.setCreditValue(2);
+		tuRepository.save(u2);
 		return "index";
 	}
 
--- src/main/java/com/rexam/dao/TeachingUnitRepository.java
+++ src/main/java/com/rexam/dao/TeachingUnitRepository.java
@@ -1,9 +1,14 @@
 package com.rexam.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.rexam.model.TeachingUnit;
 
 public interface TeachingUnitRepository extends CrudRepository<TeachingUnit, String>{
 
+	@Query(value = "select distinct discipline from TeachingUnit")
+	public List<String> findDisciplines();
 }
