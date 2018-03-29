29b0263713f5c2f6bc66b7c7737ec3c94b5cd9a3
Zakarya Moumene <zakarya.moumene@gmail.com>
2018-03-29 04:05:03 +0200

    tests

--- src/test/java/com/rexam/ResultEditionServiceTest.java
+++ src/test/java/com/rexam/ResultEditionServiceTest.java
@@ -1,73 +1,104 @@
 package com.rexam;
 
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import com.rexam.dao.RegistrationRepository;
-import com.rexam.dao.StudentRepository;
-import com.rexam.dao.TeachingUnitRepository;
-import com.rexam.model.Student;
-import com.rexam.model.TeachingUnit;
-import com.rexam.model.User;
+import com.rexam.dao.ExamRepository;
+import com.rexam.dao.ResultRepository;
+import com.rexam.model.Result;
 import com.rexam.service.RegistrationService;
+import com.rexam.service.ResultEditionService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 @Transactional
 public class ResultEditionServiceTest {
 
-	@Autowired
-	private RegistrationRepository registrationRepository;
-	@Autowired
-	private TeachingUnitRepository teachingUnitRepository;
-	@Autowired
-	private StudentRepository studentRepository;
-	
-//	@Autowired
-//	private ResultEditionService resService;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Autowired
-	private RegistrationService registrationService;
-	TeachingUnit teachingUnit = new TeachingUnit();
-	User student = new Student();
+    @Autowired
+    RegistrationService regService;
+    
+    @Autowired
+    ResultRepository rRepository;
+    
+    @Autowired
+    ExamRepository exRepository;
+    
+    @Autowired
+    ResultEditionService resService;
+   
 
-    @Before
-    public void setUp() {
-
-    }
-
+    // non vide
     @Test
-    public void saveTest() {
-
-        assertNotNull(teachingUnitRepository.findOne("ENSPHCU89"));
+    @WithMockUser(authorities = { "student", "admin" })
+    public void testShowAllUnits() throws Exception {
+        regService.registration("srowlands0@vimeo.com", "ENSPHCU89");
+        this.mockMvc.perform(get("/admin/showTU")).andDo(print())
+                .andExpect(view().name("teachingUnits"))
+                .andExpect(model().attributeExists("tuList"));
     }
-
+    // vide
     @Test
-    public void findOneStudentTest() {
-        assertNotNull(studentRepository.findOne("srowlands0@vimeo.com"));
+    @WithMockUser(authorities = { "student", "admin" })
+    public void testShowAllUnits2() throws Exception {
+        this.mockMvc.perform(get("/admin/showTU")).andDo(print())
+                .andExpect(view().name("teachingUnits"))
+                .andExpect(model().attribute("tuList", hasSize(0))
+                        );
     }
-
+    
     @Test
-    public void registrationWorkingTest() throws Exception {
-        registrationService.registration("srowlands0@vimeo.com", "ENSPHCU89");
-        registrationService.registration("rsemper2o@comsenz.com", "ENSPHCU89");
-        assertNotNull(registrationRepository.findAll());
-    }
+    public void oneRegTest() throws Exception {
+        regService.registration("srowlands0@vimeo.com", "ENSPHCU89");
 
-    @Test(expected = Exception.class)
-    public void registrationWrongTUCode() throws Exception {
-        registrationService.registration("srowlands0@vimeo.com", "inexistant");
-    }
-    @Test(expected = Exception.class)
-    public void registrationWrongMail() throws Exception {
-        registrationService.registration("srowlandzfzefzfzfzefs0@vimeo.com", "ENSPHCU89");
+        List<Result> results = new ArrayList<Result>();
+        results.addAll(rRepository.findByExam(exRepository.findOne("989")));
+        results.get(0).setScore(15.0);
+        results.get(0).setDateObtened(LocalDate.now().toString());
+
+        rRepository.save(results);
+
+        resService.computeAvg(exRepository.findOne("989"));
+        resService.updateStatus(exRepository.findOne("989"));
+        
+        results.clear();
+        results.addAll(rRepository.findByExam(exRepository.findOne("1989")));
+        results.get(0).setScore(15.0);
+        results.get(0).setDateObtened(LocalDate.now().toString());
+        rRepository.save(results);
+        
+        resService.computeAvg(exRepository.findOne("1989"));
+        resService.updateStatus(exRepository.findOne("1989"));
+        
+        results.clear();
+        results.addAll(rRepository.findByExam(exRepository.findOne("2989")));
+        results.get(0).setScore(15.0);
+        results.get(0).setDateObtened(LocalDate.now().toString());
+
+        rRepository.save(results);
+        resService.computeAvg(exRepository.findOne("2989"));
+        resService.updateStatus(exRepository.findOne("2989"));
+        
     }
 }
